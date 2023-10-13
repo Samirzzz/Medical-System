b822b793ee87ab8e6efa228aae385bf6ae7138b2
Samirzzz <abdelrahman.200300@gmail.com>
2023-10-13 15:59:40 +0300

    adduser

--- addemployees.php
+++ addemployees.php
@@ -14,12 +14,20 @@ include_once'navigation.php';
 <section class="vh-100 bg-image"
   style="background-image: url('https://mdbcdn.b-cdn.net/img/Photos/new-templates/search-box/img4.webp');">
   <div class="mask d-flex align-items-center h-100 gradient-custom-3">
-    <div class="container h-100">
-      <div class="row d-flex justify-content-center align-items-center h-100">
+    <div class="container h-100" >
+      <div class="row d-flex justify-content-center align-items-center h-100" >
         <div class="col-12 col-md-9 col-lg-7 col-xl-6">
           <div class="card" style="border-radius: 15px;">
-            <div class="card-body p-5">
-              <h2 class="text-uppercase text-center mb-5">AddUser</h2>
+            <div class="card-body p-5" style="height: 19cm;">
+              <div class="choose">
+             <div class="nav"> 
+             <a href="addemployees.php" class="bo">AddUser        </a>
+              <a href="#" class="bo">EditUser       </a>
+              <a href="#" class="bo">ViewUsers      </a>
+             </div>
+              
+              </div>
+              <h2 class="text-uppercase text-center mb-5">Add User</h2>
 
               <form>
 
