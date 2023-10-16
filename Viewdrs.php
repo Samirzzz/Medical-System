3812a2fa848f787899ee22620dbb260af3c2665b
wagiihh <ibrahim2105690@miuegypt.edu.eg>
2023-10-16 21:29:38 +0300

    view doctor basics

---
+++ Viewdrs.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="viewdrs.css">
+</head>
+<body>
+
+<!-- temp nav bar -->
+<nav class="navbar">
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+    <div class="container">
+<div class="doctor-list">
+        <div class="doctor-box">
+            <img src="test.jpg" alt="Doctor 1">
+            <div class="doctor-info">
+                <h2>Dr. John Doe</h2>
+                <p>Specialty: Cardiologist</p>
+                <p>Fee: $150</p>
+                <p>Average Waiting Time: 20 minutes</p>
+            </div>
+            <button class="bookb">Book Appointment</button>
+        </div>
+    </div>
+    
+<div class="container">
+        <div class="doctor-box">
+            <img src="test.jpg" alt="Doctor 2">
+            <div class="doctor-info">
+                <h2>Dr. Jane Smith</h2>
+                <p>Specialty: Pediatrician</p>
+                <p>Fee: $120</p>
+                <p>Average Waiting Time: 15 minutes</p>
+            </div>
+            <button class="bookb">Book Appointment</button>
+        </div>
+    </div>
+        
+        <div class="container">
+        <div class="doctor-box">
+            <img src="test.jpg" alt="Doctor 2">
+            <div class="doctor-info">
+                <h2>Dr. Jane Smith</h2>
+                <p>Specialty: Pediatrician</p>
+                <p>Fee: $120</p>
+                <p>Average Waiting Time: 15 minutes</p>
+            </div>
+            <button class="bookb">Book Appointment</button>
+        </div>
+    </div>
+        
+        <div class="container">
+        <div class="doctor-box">
+            <img src="test.jpg" alt="Doctor 2">
+            <div class="doctor-info">
+                <h2>Dr. Jane Smith</h2>
+                <p>Specialty: Pediatrician</p>
+                <p>Fee: $120</p>
+                <p>Average Waiting Time: 15 minutes</p>
+            </div>
+            <button class="bookb">Book Appointment</button>
+        </div>
+    </div>
+        
+
+        <!-- Add more doctor boxes as needed -->
+    </div>
+</body>
+</html>
