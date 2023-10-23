eaa420e16a7a289b55b3f54899a7a99c5529388c
seif2102995 <seif2102995@miuegypt.edu.eg>
2023-10-23 03:24:19 +0300

    validations for add and edit appointments

--- editappointments.php
+++ editappointments.php
@@ -1,139 +1,151 @@
 <?php
-include "includes/appnavbar.php";
 include_once "includes/db.php";
+include "includes/appnavbar.php";
+$errors = array();
 
-if (isset($_GET['Appid'])) {
-    $appid = $_GET['Appid'];
+if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['submit'])) {
+    $a_date = htmlspecialchars($_POST['date']);
+    $a_time = htmlspecialchars($_POST['time']);
+    $a_status = htmlspecialchars($_POST['status']);
 
-    // Retrieve appointment details for editing
-    $sql = "SELECT * FROM appointments WHERE Appid = $appid";
-    $result = $conn->query($sql);
-
-    if ($result->num_rows == 1) {
-        $row = $result->fetch_assoc();
-
-
-        if (isset($_POST['submit'])) {
-            // Retrieve form data and perform necessary validation
-            $date = $_POST['date'];
-            $time = $_POST['time'];
-            $status = $_POST['status'];
-    
-            // Perform the update in the database
-            $update_sql = "UPDATE appointments SET date = '$date', time = '$time', status = '$status' WHERE Appid = '$appid'";
-         $ress = mysqli_query($conn, $update_sql);  
-           if($ress){
-            echo "updated successfuly";
-           }
-           else 
-           {
-            echo "error in updating"  ;
-           }
-    
-       
-    
-            
-        }
-
-
-
-
-
-
-
-        
-        // Display the edit form here
-        // Populate the form fields with $row data for editing
-    } else {
-        echo "Appointment not found.";
+    // Validation for required fields
+    if (empty($a_date)) {
+        $errors[] = "Date is required";
     }
-} else {
-    echo "Invalid appointment ID.";
-}?>
 
-<!-- Display the edit form here -->
+    if (empty($a_time)) {
+        $errors[] = "Time is required";
+    }
+
+    if (empty($a_status)) {
+        $errors[] = "Status is required";
+    } else {
+        // Date validation
+        $currentDate = date("Y-m-d");
+        $maxAllowedDate = date("Y-m-d", strtotime("+45 days")); // 1.5 months ahead
+
+        if ($a_date < $currentDate || $a_date > $maxAllowedDate) {
+            $errors[] = "Date must be between today and 1.5 months ahead.";
+        }
+    }
+
+    if (count($errors) === 0) {
+        // Process form data here
+
+        $sql = "INSERT INTO appointments (date, time, status, pid, did, cid) VALUES ('$a_date', '$a_time', '$a_status', '40', '430', '540')";
+        $res = mysqli_query($conn, $sql);
+
+        if ($res) {
+            echo "Form submitted successfully!";
+        } else {
+            echo "Error: " . mysqli_error($db);
+        }
+    } else {
+        // Display validation errors
+        echo "Validation Errors:<br>";
+        foreach ($errors as $error) {
+            echo $error . "<br>";
+        }
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   
-    <title>Document</title>
+    <title>Another Form</title>
+    <style>
+        /* Style for the overall form container */
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f5f5f5;
+        }
+
+        form {
+            background-color: #fff;
+            border: 1px solid #ccc;
+            padding: 20px;
+            width: 300px;
+            margin: 0 auto;
+            margin-top: 50px;
+            border-radius: 5px;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+        }
+
+        /* Style for form labels */
+        label {
+            display: block;
+            margin-bottom: 5px;
+        }
+
+        /* Style for input fields */
+        input[type="text"],
+        input[type="date"],
+        input[type="submit"] {
+            width: 100%;
+            padding: 10px;
+            margin-bottom: 10px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            transition: border-color 0.3s;
+        }
+
+        input[type="text"]:focus,
+        input[type="date"]:focus,
+        input[type="submit"] {
+            border-color: #333;
+            outline: none;
+        }
+
+        /* Style for form submit button */
+        input[type="submit"] {
+            background-color: #333;
+            color: #fff;
+            padding: 10px 20px;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+            transition: background-color 0.3s;
+        }
+
+        input[type="submit"]:hover {
+            background-color: #555;
+        }
+    </style>
 </head>
 <body>
-   <form action="" method = 'post' autocomplete="off">
-    <label for="d">date</label>
-    <input type="date"  placeholder="choose the date" id="d" name="date">
+   <form action="" method="post" autocomplete="off" onsubmit="return validateForm();">
+    <label for="d">Date</label>
+    <input type="date" placeholder="Choose the date" id="d" name="date">
     <br>
-    <label for="t">time</label>
-    <input type="text" placeholder ="enter the time" id="t" name="time">
+    <label for="t">Time</label>
+    <input type="text" placeholder="Enter the time" id="t" name="time">
     <br>
-    <label for="s">status</label>
-    <input type="text" placeholder ="enter status " id="s" name="status">
+    <label for="s">Status</label>
+    <input type="text" placeholder="Enter status" id="s" name="status">
     <br>
     <input type="submit" id="submit" name="submit">
+   </form>
+   <script>
+    function validateForm() {
+        var dateInput = document.getElementById("d");
+        var currentDate = new Date();
+        var maxAllowedDate = new Date();
+        maxAllowedDate.setDate(maxAllowedDate.getDate() + 45); // 1.5 months ahead
 
-    
-   </form> 
-   <style>
-    /* Style for the overall form container */
-body {
-    font-family: Arial, sans-serif;
-    background-color: #f5f5f5;
-}
+        if (dateInput.value === "") {
+            alert("Date is required");
+            return false;
+        }
 
-form {
-    background-color: #fff;
-    border: 1px solid #ccc;
-    padding: 20px;
-    width: 300px;
-    margin: 0 auto;
-    margin-top: 50px;
-    border-radius: 5px;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-}
-
-/* Style for form labels */
-label {
-    display: block;
-    margin-bottom: 5px;
-}
-
-/* Style for input fields */
-input[type="text"],
-input[type="date"] ,
-input[type="submit"] 
-{
-    width: 100%;
-    padding: 10px;
-    margin-bottom: 10px;
-    border: 1px solid #ccc;
-    border-radius: 5px;
-    transition: border-color 0.3s;
-}
-
-input[type="text"]:focus,
-input[type="date"]:focus,
-input[type="submit"]  {
-    border-color: #333;
-    outline: none;
-}
-
-/* Style for form submit button */
-input[type="submit"] {
-    background-color: #333;
-    color: #fff;
-    padding: 10px 20px;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
-    transition: background-color 0.3s;
-}
-
-input[type="submit"]:hover {
-    background-color: #555;
-}
-
-   </style>
+        var selectedDate = new Date(dateInput.value);
+        if (selectedDate < currentDate || selectedDate > maxAllowedDate) {
+            alert("Date must be between today and 1.5 months ahead.");
+            return false;
+        }
+    }
+</script>
 </body>
 </html>
