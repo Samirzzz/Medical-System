60d72d28790eb493a0c89c549271d558be4953e8
arafat3300 <125751269+arafat3300@users.noreply.github.com>
2023-10-25 00:05:52 +0300

    home

--- home.php
+++ home.php
@@ -4,10 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="homepage.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Anton&family=Bebas+Neue&family=Poppins:wght@300;600&family=Righteous&family=Rubik:ital@1&display=swap" rel="stylesheet">
     <title>hello</title>
 
 </head>
 <body>
+
 <?php  include_once'Navbar.php';  ?>
 
 
@@ -35,14 +40,31 @@
                     </ul>
                 </div> 
 </div>
+<section class="locations-in"></section>
+<h1 id="clinc-title"> Our Locations</h1>
+       
+<div class="clinc-locs">
+<div class= "Maps-loc">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3455.706381893326!2d31.41503607569206!3d29.987867174952235!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14583c9e7cd14479%3A0x48c22f61efd619f1!2sPrime%20Clinics!5e0!3m2!1sen!2seg!4v1698062329978!5m2!1sen!2seg" width="280" height="280" style="border: 4px solid black; border-radius:16px; margin-right:100px" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <h6 class= "location_name">Prime clinc Location</h6>
+         </div>
 
-       <div class= Maps-loc>
-             <h6>Prime clinc Location</h6>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3455.706381893326!2d31.41503607569206!3d29.987867174952235!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14583c9e7cd14479%3A0x48c22f61efd619f1!2sPrime%20Clinics!5e0!3m2!1sen!2seg!4v1698062329978!5m2!1sen!2seg" width="450" height="450" style="border:0; margin-right:100px" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-            </div>
+         <div class= "Maps-loc">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3455.706381893326!2d31.41503607569206!3d29.987867174952235!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14583c9e7cd14479%3A0x48c22f61efd619f1!2sPrime%20Clinics!5e0!3m2!1sen!2seg!4v1698062329978!5m2!1sen!2seg" width="280" height="280" style="border: 4px solid black; border-radius:16px; margin-right:100px" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <h6 class= "location_name">Prime clinc Location</h6>
+         </div>
 
+         <div class= "Maps-loc">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3455.706381893326!2d31.41503607569206!3d29.987867174952235!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14583c9e7cd14479%3A0x48c22f61efd619f1!2sPrime%20Clinics!5e0!3m2!1sen!2seg!4v1698062329978!5m2!1sen!2seg" width="280" height="280" style="border: 4px solid black; border-radius:16px; margin-right:100px" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <h6 class= "location_name">Prime clinc Location</h6>
+         </div>
 
+         <div class= "Maps-loc">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3455.706381893326!2d31.41503607569206!3d29.987867174952235!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14583c9e7cd14479%3A0x48c22f61efd619f1!2sPrime%20Clinics!5e0!3m2!1sen!2seg!4v1698062329978!5m2!1sen!2seg" width="280" height="280" style="border: 4px solid black; border-radius:16px; margin-right:100px" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <h6 class= "location_name">Prime clinc Location</h6>
+         </div>
 
+</div>
 
 
 
