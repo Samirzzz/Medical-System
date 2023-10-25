fe063e7b0011589cf372fffa519c251b15df0673
wagiihh <ibrahim2105690@miuegypt.edu.eg>
2023-10-26 01:57:21 +0300

    edit form validation and delete user

--- Editprofile.php
+++ Editprofile.php
@@ -48,7 +48,7 @@
                 $_SESSION["education"] = $Education;
             }
 
-           
+            echo "User updated successfully";
         } else {
             echo "Error: " . mysqli_error($conn);
         }
@@ -58,19 +58,21 @@
 <body>
     <div class="container mt-4">
         <h1>Edit Profile</h1>
-        <form action="" method="post">
+        <form action="" method="post" onsubmit="return validateForm()">
             <div class="form-group">
                 <label for="firstname">First Name</label>
                 <input type="text" class="form-control" id="firstname" name="firstname" value="<?php echo $_SESSION["firstname"]; ?>">
+                <div class="error-message" id="fname-error"></div>
             </div>
             <div class="form-group">
                 <label for "lastname">Last Name</label>
                 <input type="text" class="form-control" id="lastname" name="lastname" value="<?php echo $_SESSION["lastname"]; ?>">
+                <div class="error-message" id="lname-error"></div>
             </div>
-
             <div class="form-group">
                 <label for="number">Phone</label>
                 <input type="tel" class="form-control" id="number" name="number" value="<?php echo $_SESSION["number"]; ?>">
+                <div class="error-message" id="phone-error"></div>
             </div>
 
             <?php
@@ -79,22 +81,61 @@
                     <div class="form-group">
                         <label for="address">Address</label>
                         <input type="text" class="form-control" id="address" name="address" value="' . $_SESSION["address"] . '">
+                        <div class="error-message" id="address-error"></div>
                     </div>';
             } elseif ($_SESSION["type"] == 'doctor') {
                 echo '<div class="form-group">
                         <label for="specialization">Specialization</label>
                         <input type="text" class="form-control" id="specialization" name="specialization" value="' . $_SESSION["specialization"] . '">
+                        <div class="error-message" id="specialization-error"></div>
                     </div>
                     <div class="form-group">
                         <label for="education">Education</label>
                         <input type="text" class="form-control" id="education" name="education" value="' . $_SESSION["education"] . '">
+                        <div class="error-message" id="education-error"></div>
                     </div>';
             }
             ?>
-
+            
             <button type="submit" class="btn btn-primary">Save Changes</button>
         </form>
     </div>
+
+    <script>
+        function validateForm() {
+            // Reset error messages
+            var errorElements = document.querySelectorAll(".error-message");
+            for (var i = 0; i < errorElements.length; i++) {
+                errorElements[i].innerHTML = "";
+            }
+
+            var fname = document.getElementById("firstname").value;
+            var lname = document.getElementById("lastname").value;
+            var phone = document.getElementById("number").value;
+
+            var isValid = true;
+
+            if (fname === "") {
+                document.getElementById("fname-error").innerHTML = "First Name is required.";
+                isValid = false;
+            }
+            if (lname === "") {
+                document.getElementById("lname-error").innerHTML = "Last Name is required.";
+                isValid = false;
+            }
+            if (phone === "") {
+                document.getElementById("phone-error").innerHTML = "Phone Number is required.";
+                isValid = false;
+            }
+
+            if (phone.length !== 11) {
+                document.getElementById("phone-error").innerHTML = "Invalid phone number. It should be 11 digits.";
+                isValid = false;
+            }
+
+            return isValid;
+        }
+    </script>
 </body>
 <footer>
     <?php
--- profile.php
+++ profile.php
@@ -10,6 +10,25 @@
     <?php
     session_start();
     include_once 'Navbar.php';
+    include_once 'includes/db.php';
+
+    if ($_SERVER['REQUEST_METHOD'] == "POST") {
+        $x = $_SESSION['email'];
+        $UserType = $_SESSION["type"];
+
+        if ($UserType == 'patient') {
+            $sql = "DELETE FROM patient WHERE uid = " . $_SESSION['ID'];
+        } elseif ($UserType == 'doctor') {
+            $sql = "DELETE FROM dr WHERE uid = " . $_SESSION['ID'];
+        }
+
+        $result = mysqli_query($conn, $sql);
+
+        if ($result) {
+            echo 'User deleted successfully';
+            header("location:home.php");
+        }
+    }
     ?>
 </header>
 <body>
@@ -30,6 +49,10 @@
         ?>
         
         <a class="btn btn-primary" href="Editprofile.php">Edit Profile</a>
+   
+        <form action="" method="post">
+            <button class="btn btn-danger" name="delete" onclick="return confirm('Are you sure you want to delete your profile?');">Delete Profile</button>
+        </form>
     </div>
 </body>
 <footer>
