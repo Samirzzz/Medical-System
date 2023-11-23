98e92ae3021e26abded2d2c34b829248ff64ac80
Samirzzz <abdelrahman.200300@gmail.com>
2023-11-23 21:16:36 +0200

    patientclass

--- views/classes.php
+++ views/classes.php
@@ -1,11 +1,10 @@
 <?php 
 include_once '..\includes\db.php';
 class user{
-    public $username;
+    public $email;
 	public $pass;
 	public $type;
 	public $id;
-	public $email;
 
 function __construct($id)
 {
@@ -13,7 +12,7 @@ if($id!=""){
     $sql="select * from user_acc where uid=$id";
     $result = mysqli_query($GLOBALS['conn'],$sql);
 if($row=mysqli_fetch_array($result)){
-                $this->username=$row["username"];
+                $this->email=$row["email"];
 				$this->pass=$row["pass"];
 				$this->id=$row["uid"];
 				$this->type=$row['type'];
@@ -23,9 +22,6 @@ if($row=mysqli_fetch_array($result)){
 
 
 }
-function getallpatients(){
-    
-}
 
 
 
@@ -35,8 +31,72 @@ function getallpatients(){
 
 }
 
-class clinic{
+class Patient extends user{
+	public $firstname;
+	public $lastname;
+	public $age;
+	public $gender;
+	public $address;
+	public $number;
+	public $uid;
+	public $pid;
 
+	
+	function __construct($id)
+	{
+$sql = "SELECT user_acc.uid, user_acc.email,user_acc.type, patient.firstname, patient.lastname,patient.gender,
+	patient.address,patient.number,patient.age,patient.uid,patient.pid 
+	FROM patient 
+	JOIN user_acc ON user_acc.uid = patient.uid where user_acc.uid=".$id;
+			$result = mysqli_query($GLOBALS['conn'],$sql);
+	        if($row=mysqli_fetch_array($result)){
+				parent::__construct($row["uid"]);
+				$this->pid=$row["pid"];
+
+	             	$this->firstname=$row["firstname"];
+	             	$this->lastname=$row["lastname"];
+	 				$this->uid=$row["uid"];
+
+
+					
+					
+
+					
+
+	
+	// $sql = "SELECT user_acc.uid, user_acc.email,user_acc.type, patient.firstname, patient.lastname,patient.gender,
+	// patient.address,patient.number,patient.age,patient.uid 
+	// FROM patient 
+	// JOIN user_acc ON user_acc.uid = patient.uid";
+	// 	$result = mysqli_query($GLOBALS['conn'],$sql);
+	
+	}
+
+	
+}
+	static function patientsearch($value)  {
+		$i=0;
+		$patient=array();
+		$sql="SELECT user_acc.uid, user_acc.email, patient.firstname, patient.lastname,patient.pid   
+		FROM patient
+		JOIN user_acc ON user_acc.uid = patient.uid 
+		WHERE email LIKE '%$value%'";
+		$result = mysqli_query($GLOBALS['conn'],$sql);
+
+		while($row=mysqli_fetch_array($result)) {
+			$patient[$i++]=new Patient($row[0]);
+		}	
+		return $patient;
+
+	}
+	
+	
+
+
+
+
+
+	
 }
 
 
--- views/clivesearch.php
+++ views/clivesearch.php
@@ -1,6 +1,12 @@
 <link rel="stylesheet" href="..\public\css\clive.css">
+<style>
+        [id^="plink"] {
+            display: none;
+        }
+</style>
 <?php
 include("..\includes\db.php");
+include(".\classes.php");
 
 if (isset($_POST["query"])) {
     $usertype = $_POST['type'];
@@ -79,41 +85,35 @@ if (isset($_POST["query"])) {
                 // } 
     }
                  elseif ($usertype == 'patients') {
-                    $sql = "SELECT user_acc.uid, user_acc.email, patient.firstname, patient.lastname,patient.pid 
-                            FROM patient 
-                            JOIN user_acc ON user_acc.uid = patient.uid 
-                            WHERE email LIKE '%$search%'";
-                               $result = $conn->query($sql);
-
-                               echo '<form action="" method="post">';
-                               if ($result->num_rows > 0) {
-                                   echo '<div id="search-result">';
-                                   echo '<table>';
-                                   echo '<tr>';
-                                   echo '<th>ID</th>';
-                                   echo '<th>email</th>';
-                                   echo '<th>Name</th>';
-                                   echo '</tr>';
+                    $viewpatients = Patient::patientSearch($search);
+    if (!empty($viewpatients)) {
+        echo '<div id="search-results">';
+        echo '<table>';
+        echo '<tr>';
+        echo '<th>ID</th>';
+        echo '<th>Email</th>';
+        echo '<th>First Name</th>';
+        echo '<th>Last Name</th>';
+        echo '</tr>';
+        foreach ($viewpatients as $pat) {
+            echo '<tr>';
+                    echo '<td><a  id="pop' . $pat->uid . '" href="#plink' . $pat->uid . '">' . $pat->pid . '</a></td>';
+                    echo '<td><a  id="pop1' . $pat->uid . '" href="#plink' . $pat->uid . '">' . $pat->email . '</a></td>';
+                    echo '<td><a  id="pop2' . $pat->uid . '" href="#plink' . $pat->uid . '">' . $pat->firstname . '</a></td>';
+                    echo '<td><a  id="pop3' . $pat->uid . '" href="#plink' . $pat->uid . '">' . $pat->lastname . '</a></td>';
+                    echo '</tr>';
                        
-                                   while ($row = $result->fetch_array()) {
-                                       echo '<tr>';
-                                       echo '<td>' . '<a id="pop"  href="#plink?uid=' . $row["uid"] . '">' . $row["pid"] . '</a>' . '</td>';
-                                       echo '<td>' . '<a id="pop1" href="#plink?uid=' . $row["uid"] . '">' . $row["email"] . '</a>' . '</td>';
-                                       echo '<td>' . '<a id="pop2" href="#plink?uid=' . $row["uid"] . '">' . $row["firstname"] .$row["lastname"].  '</a>' . '</td>';
-                                      
-                                       echo '</tr>';
-                       
-                                       echo '<div id="plink">';
+                                       echo '<div id="plink'. $pat->uid . '">';
                                        echo '<div class="container mt-5 d-flex justify-content-center">';
                                        echo '<div class="card p-3" style="width: 300px;"> ';
                                        echo '<div class="d-flex align-items-center">';
                                        echo '<div class="ml-3 w-100">';
-                                       echo '<h4 class="mb-0 mt-0">' . $row["firstname"] . '</h4>';
-                                       echo '<span>' . $row["lastname"] . '</span>';
+                                       echo '<h4 class="mb-0 mt-0">' . $pat->firstname . '</h4>';
+                                       echo '<span>' . $pat->lastname  . '</span>';
                                        echo '<div class="p-2 mt-2 bg-primary d-flex justify-content-between rounded text-white stats">';
                                        echo '<div class="d-flex flex-column">';
                                        echo '<span class="articles">Patient Mail</span>';
-                                       echo '<span class="number1">'.$row['email'].'</span>';
+                                       echo '<span class="number1">'.$pat->email .'</span>';
                                        echo '</div>';
                                       
                                        echo '<div class="d-flex flex-column">';
@@ -122,9 +122,9 @@ if (isset($_POST["query"])) {
                                        echo '</div>';
                                        echo '<div class="button mt-2 d-flex flex-row align-items-center">';
                                        
-                                       echo '<button type="submit" name="deletee" class="btn btn-sm btn-outline-primary w-100"><a href=".\deletepatients.php?uid=' . $row["uid"] . '">Delete</a></button>';
+                                       echo '<button type="submit" name="deletee" class="btn btn-sm btn-outline-primary w-100"><a href=".\deletepatients.php?uid=' .$pat->uid . '">Delete</a></button>';
                                        echo  '</form>';
-                                       echo '<button class="btn btn-sm btn-primary w-100 ml-2" id="close">Close</button>';
+                                       echo '<button class="btn btn-sm btn-primary w-100 ml-2" id="close'.$pat->uid.'">Close</button>';
                        
                                        echo '</div>';
                                        echo '</div>';
@@ -213,16 +213,21 @@ if (isset($_POST["query"])) {
 ?>
 
 <script>
- $(document).ready(function () {
-        $("#pop, #pop1, #pop2, #pop3").click(function () {
-            $("#plink").show();
-        });
+    $(document).ready(function () {
+        <?php
+        foreach ($viewpatients as $pat) {
+            echo '$("#pop' . $pat->uid . ', #pop1' . $pat->uid . ', #pop2' . $pat->uid . ', #pop3' . $pat->uid . '").click(function () {';
+            echo '$("#plink' . $pat->uid . '").show();';
+            echo '});';
 
-        $("#close").click(function (e) {
-            e.preventDefault();  
-
-            $("#plink").hide();
-        });
+            echo '$("#close' . $pat->uid . '").click(function (e) {';
+            echo 'e.preventDefault();';
+            echo '$("#plink' . $pat->uid . '").hide();';
+            echo '});';
+        }
+        ?>
     });
 </script>
+
+
 </html>
