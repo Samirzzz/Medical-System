964e482fbde754c0affafeb779a35c3c830cee8e
seif2102995 <seif2102995@miuegypt.edu.eg>
2023-12-18 23:36:45 +0200

    eav

--- views/patientinfo.php
+++ views/patientinfo.php
@@ -9,17 +9,41 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body >
-<script>
 
-
-
-
-</script>
 
     <?php 
     include_once '..\includes\navigation.php';
   
-    if (isset($_GET['uid'])) {
+$treat_id=null;
+    $sql = "select treat_id , treat_name from treatment";
+    $res = mysqli_query($conn,$sql);
+    $treat_opt=[];
+    while($row=mysqli_fetch_assoc($res)){
+        $treat_opt [] = [
+            'treat_id' => $row['treat_id'],
+            'treat_name'=>$row['treat_name'],
+            
+        ];
+    }
+
+    $sql = "select id , opt_id from d_s_o where treat_id ='$treat_id'";
+    $res = mysqli_query($conn,$sql);
+    $treat_opt=[];
+    while($row=mysqli_fetch_assoc($res)){
+        $treat_opt [] = [
+            'opt_id' => $row['opt_id'],
+            'treat_name'=>$row['id'],
+            
+        ];
+    }
+   
+
+
+
+
+
+
+    if (isset($_GET['uid'])) {    // get uid
         $uid = $_GET['uid'];
     
         $sql = "SELECT
@@ -55,7 +79,7 @@
         <div class="col-md-5 border-right">
             <div class="p-3 py-5">
                 <div class="d-flex justify-content between align-items-center mb-3">
-                    <h4 class="text-right">Patient Profile</h4>
+                    <h4 class="text-right">Patient Profile</h4> 
                 </div>
                 <form action="" method="post">
                         <div class="row mt-2">
@@ -100,12 +124,35 @@
             <div class="p-3 py-5">
                 <div class="col-md-12">
                     <label class="labels" style="font-size: larger;">Medical History</label>
+                    <br>
+                    <br>
+                    <form action=""> 
+        <label for="diagnosename">Diagnose name</label>
+    <input type="text" placeholder="diagnose name" id="diagnosename" name="diagnosename">
+    <br>
+    <br>
+    <label for="treatment_id">Treatment</label>
+     <select id="treatment_id" name="treatment_id" onchange="treat_opt(event)">
+        <?php foreach ($treat_opt as $opt) { ?>
+            <option value="<?php echo $opt['treat_id']; ?>">
+                <?php echo $opt['treat_name'] ; ?>
+            </option>
+        <?php } ?>
+   
+      
+    <input type="hidden" id="uid" name="uid" value="<?php echo $uid?>"> 
+    <!-- <input type="submit" value="treatment-options"> -->
+
+
+          
+       </form>    
                     <div id="d1">
-        <input type="button" value="Create new" style="width:200px" name="t" id="t"   onclick="create();">    
+        <!-- <input type="button" value="Create new" style="width:200px" name="t" id="t"   onclick="create();"> -->
+
 
     </div>
-    <div>
-    <table class="table mt-3" style="margin-left: -40px;">
+    <!-- <div> -->
+    <!-- <table class="table mt-3" style="margin-left: -40px;">
                         <thead>
                             <tr>
                                 <th>Diagnosis</th>
@@ -113,26 +160,26 @@
                         </thead>
                         <tbody>
                             <?php
-                                $sqlPid = "SELECT Pid,uid FROM patient WHERE uid = '$uid'";
-                                $resultPid = mysqli_query($conn, $sqlPid);       
-                                if ($rowPid = mysqli_fetch_array($resultPid)) {
-                                    $uid = $rowPid['uid'];               
-                                $sqlDiagnosis = "SELECT diagnosis,date,uid FROM medications WHERE uid = '$uid'";
-                                $resultDiagnosis = mysqli_query($conn, $sqlDiagnosis);
+                            //     $sqlPid = "SELECT Pid,uid FROM patient WHERE uid = '$uid'";
+                            //     $resultPid = mysqli_query($conn, $sqlPid);       
+                            //     if ($rowPid = mysqli_fetch_array($resultPid)) {
+                            //         $uid = $rowPid['uid'];               
+                            //     $sqlDiagnosis = "SELECT diagnosis,date,uid FROM medications WHERE uid = '$uid'";
+                            //     $resultDiagnosis = mysqli_query($conn, $sqlDiagnosis);
                                 
                                 
-                                while ($row = mysqli_fetch_array($resultDiagnosis)) {
-                                    echo '<tr>';
-                                    echo '<td>' . $row["diagnosis"] . '</td>';
-                                    echo '<td>' . $row["date"] . '</td>';
+                            //     while ($row = mysqli_fetch_array($resultDiagnosis)) {
+                            //         echo '<tr>';
+                            //         echo '<td>' . $row["diagnosis"] . '</td>';
+                            //         echo '<td>' . $row["date"] . '</td>';
 
-                                    echo '</tr>';
-                                }
-                            }
+                            //         echo '</tr>';
+                            //     }
+                            // }
                             ?>
                         </tbody>
                     </table>
-    </div>
+    </div> -->
 
                 </div>
                 <br>
@@ -179,94 +226,102 @@ if($_SERVER['REQUEST_METHOD']== "POST"){
 
     $result = mysqli_query($conn, $sql);
     }
-    // if ($result) {
-    //     if (isset($_POST["updateEmail"])) {
-    //         $_SESSION['email'] = $email;
-    //         echo "Email updated";
-    //     } elseif (isset($_POST["updatePassword"])) {
-    //         $_SESSION['password'] = $pass;
-    //         echo "Password updated";
-    //     }
-    //}
-    //  else {
-    //     echo "Invalid input";
-    // }
+    if ($result) {
+        if (isset($_POST["updateEmail"])) {
+            $_SESSION['email'] = $email;
+            echo "Email updated";
+        } elseif (isset($_POST["updatePassword"])) {
+            $_SESSION['password'] = $pass;
+            echo "Password updated";
+        }
+    }
+     else {
+        echo "Invalid input";
+    }
 
 }
 
 ?>
 </body>
 <script>
-    var counter = 1;
+//     var counter = 1;
 
-function create() {
-    var newButton = document.createElement("input");
-    newButton.type = "button";
-    newButton.value = "Add";
-    newButton.id="tt";
-    newButton.style.width = "100px";
-    document.getElementById("t").onclick = null;
-    newButton.onclick = function () {
-        createNewInput();
-    document.getElementById("tt").onclick = null;
+// function create() {
+//     var newButton = document.createElement("input");
+//     newButton.type = "button";
+//     newButton.value = "Add";
+//     newButton.id="tt";
+//     newButton.style.width = "100px";
+//     document.getElementById("t").onclick = null;
+//     newButton.onclick = function () {
+//         createNewInput();
+//     document.getElementById("tt").onclick = null;
 
-    };
+//     };
 
-    var d = document.getElementById("d1");
-    d.appendChild(newButton);
-    d.appendChild(document.createElement("br"));
-}
+//     var d = document.getElementById("d1");
+//     d.appendChild(newButton);
+//     d.appendChild(document.createElement("br"));
+// }
 
-function createNewInput() {
-    var d = document.getElementById("d1");
+// function createNewInput() {
+//     var d = document.getElementById("d1");
 
-    var newForm = document.createElement("form");
-    newForm.setAttribute("method", "post");
+//     var newForm = document.createElement("form");
+//     newForm.setAttribute("method", "post");
 
-    var newInput = document.createElement("input");
-    newInput.type = "text";
-    newInput.name = "input_1";
-    newInput.id = "inp";
-    newInput.placeholder = "Type here";
-    newInput.style.width = "300px";
-    newInput.style.height = "30px";
+//     var newInput = document.createElement("input");
+//     newInput.type = "text";
+//     newInput.name = "input_1";
+//     newInput.id = "inp";
+//     newInput.placeholder = "Type here";
+//     newInput.style.width = "300px";
+//     newInput.style.height = "30px";
 
-    var newSub = document.createElement("input");
-    newSub.type = "submit";
-    newSub.name = "s_1";
-    newForm.appendChild(newInput);
-    newForm.appendChild(document.createElement("br"));
-    newForm.appendChild(newSub);
-    d.appendChild(newForm);
-    d.appendChild(document.createElement("br"));
-}
+//     var newSub = document.createElement("input");
+//     newSub.type = "submit";
+//     newSub.name = "s_1";
+//     newForm.appendChild(newInput);
+//     newForm.appendChild(document.createElement("br"));
+//     newForm.appendChild(newSub);
+//     d.appendChild(newForm);
+//     d.appendChild(document.createElement("br"));
+// }
 
 
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+// <?php
+// if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    if (isset($_GET['uid'])) {
-        $uid = $_GET['uid'];
+//     if (isset($_GET['uid'])) {
+//         $uid = $_GET['uid'];
 
-        if (isset($_POST["s_1"])) {
-            $diagnosis = htmlspecialchars($_POST["input_1"]); 
+//         if (isset($_POST["s_1"])) {
+//             $diagnosis = htmlspecialchars($_POST["input_1"]); 
 
-            $sqlPid = "SELECT Pid,uid FROM patient WHERE uid = '$uid'";
-            $resultPid = mysqli_query($conn, $sqlPid);
+//             $sqlPid = "SELECT Pid,uid FROM patient WHERE uid = '$uid'";
+//             $resultPid = mysqli_query($conn, $sqlPid);
 
-            if ($rowPid = mysqli_fetch_array($resultPid)) {
-                $uid = $rowPid['uid'];
+//             if ($rowPid = mysqli_fetch_array($resultPid)) {
+//                 $uid = $rowPid['uid'];
 
-                $sql2 = "INSERT INTO medications (diagnosis, uid) VALUES ('$diagnosis', '$uid')";
-                $result = mysqli_query($conn, $sql2);
+//                 $sql2 = "INSERT INTO medications (diagnosis, uid) VALUES ('$diagnosis', '$uid')";
+//                 $result = mysqli_query($conn, $sql2);
 
                 
-            } 
-        }
-    }
-}
+//             } 
+//         }
+//     }
+// }
 
-?>
+// ?>
 
+    </script>
+ <script>
+        document.getElementById("treatment_id").addEventListener("change", function() {
+            var selectedValue = this.value;
+            
+            // Assuming you have a second form with an input field with the id "second_input"
+            document.getElementById("dso").value = selectedValue;
+        });
     </script>
 </html>
