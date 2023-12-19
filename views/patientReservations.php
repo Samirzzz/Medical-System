dc1bbc4b00b4600a856b785dc680245e49b05598
arafat3300 <125751269+arafat3300@users.noreply.github.com>
2023-12-20 01:37:10 +0200

    test

--- views/patientReservations.php
+++ views/patientReservations.php
@@ -61,20 +61,4 @@ try {
 };
 header("location:./pindex.php");
 
-
-
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
-</head>
-<body>
-<table >
-   
-
-    
-</body>
-</html>
