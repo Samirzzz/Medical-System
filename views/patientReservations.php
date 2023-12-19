33e9b3f1b25e39d4b80b1596cb6c6c51e61b94ad
arafat3300 <125751269+arafat3300@users.noreply.github.com>
2023-12-20 01:24:37 +0200

    pateint booking confirmation

--- views/patientReservations.php
+++ views/patientReservations.php
@@ -1,6 +1,15 @@
 <?php
  session_start();
+ use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require '../PHPMailer/src/Exception.php';
+require '../PHPMailer/src/PHPMailer.php';
+require '../PHPMailer/src/SMTP.php';
 require_once '../app\controller\AppointmentController.php';
+
+
         $db = Database::getInstance();
         $conn = $this->db->getConnection();
 $appointmentcntrl =new AppointmentController();
@@ -12,6 +21,44 @@ $patientId = $appointmentcntrl->getPatientID( $_SESSION["Pid"]);
 echo "patient id : ".$patientId;
 echo "appointment id : ".$appID;
 $appointmentcntrl->bookForPatient($patientId,$appID);
+
+$curr_email=$_SESSION[$email];
+if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit']))
+{
+
+$email = $_POST["email"];
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings                 
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'tabibii.application@gmail.com';                     //SMTP username
+    $mail->Password   = 'sxxy bsog qtdc bhbd';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom($curr_email,'Tabibi Application');
+    $mail->addAddress($curr_email);     //Add a recipient             //Name is optional
+
+    // //Attachments
+    // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Booking Confirmed ';
+    $mail->Body    = 'Thank you for booking Your appointment on tabibi application your appointment has been confirmed ';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+};
 header("location:./pindex.php");
 
 
