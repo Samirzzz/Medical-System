097933de623b36614133c7e5d5bf9edcdbffe8cf
Samirzzz <abdelrahman.200300@gmail.com>
2023-12-20 06:15:29 +0200

    bug

--- views/login.php
+++ views/login.php
@@ -97,7 +97,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
             header("Location: adminmain.php");
             exit();
         } elseif ($UserObject instanceof Clinic) {
-            $_SESSION["Cid"] = $clinicCid; 
+            $_SESSION["Cid"] = $UserObject->id; 
             $_SESSION["cname"] = $UserObject->cname;
             $_SESSION["cloc"] = $UserObject->cloc;
             $_SESSION["workhrs"] = $UserObject->workhrs;
